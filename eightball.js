4a69826dc040355e14c0daecfe458521c1b5053b
yenmangu <robert.shelford@googlemail.com>
2022-04-29 01:32:30 +0100

    removed in built question

--- eightball.js
+++ eightball.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
-//eightball
 let userName = '';
 console.log(`The name you have entered is: ${userName ? userName : 'Error! No name! I will call you stranger'}.`);
 console.log(`hello ${userName ? userName : 'stranger'}! Welcome to the show!`);
-let userQ = 'will i be rich?';
+//enter your own question between the ''
+let userQ = '';
+
 console.log(`So, ${userName ? userName : 'stranger'}, you have asked the magic eight ball "${userQ ? userQ : 'nothing!'}" `)
 let randomNo = Math.floor(Math.random() * 8);
 let eightBall = '';
@@ -37,44 +37,4 @@ switch (randomNo) {
     console.log('uh oh, something has gone wrong.');
     break;
 }
-=======
-//eightball
-let userName = '';
-console.log(`The name you have entered is: ${userName ? userName : 'Error! No name! I will call you stranger'}.`);
-console.log(`hello ${userName ? userName : 'stranger'}! Welcome to the show!`);
-let userQ = 'will i be rich?';
-console.log(`So, ${userName ? userName : 'stranger'}, you have asked the magic eight ball "${userQ ? userQ : 'nothing!'}" `)
-let randomNo = Math.floor(Math.random() * 8);
-let eightBall = '';
-
-switch (randomNo) {
-  case 0:
-    eightBall = 'It is certain';
-    break;
-  case 1:
-    eightBall = 'It is decidedly so';
-    break;
-  case 2:
-    eightBall = 'Reply hazy try again';
-    break;
-  case 3:
-    eightBall = 'Cannot predict now';
-    break;
-  case 4:
-    eightBall = 'Do not count on it';
-    break;
-  case 5:
-    eightBall = 'My sources say no';
-    break;
-  case 6:
-    eightBall = 'Outlook not so good';
-    break;
-  case 7:
-    eightBall = 'Signs point to yes';
-    break;
-  default: 
-    console.log('uh oh, something has gone wrong.');
-    break;
-}
->>>>>>> a649c9a36eaccdbb8a59def95a1701cf51c7f6ef
 console.log(eightBall);
